4050a2297dc3bed9e027154ea56927d21df1fc3a
Prosper1030 <1030railgunjudgelight@gmail.com>
2024-08-22 00:59:20 +0800

    Send an email with titles and description

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
-
+from send_email import send_email
 api_key ="2abfd9f0bfdc4a428029254b35b19434"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2024-07-21&sortBy=publishedAt&apiKey=2abfd9f0bfdc4a428029254b35b19434"
 
 request = requests.get(url)
 content = request.json()
 
+body = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    title = article["title"] if article["title"] is not None else ""
+    description = article["description"] if article["description"] is not None else ""
+    body = body + title + "\n" + description + 2 * "\n"
+
+send_email(body)
---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+import ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "1030railgunjudeglight@gmail.com"
+    password = os.getenv("PASSWORD")   # 從環境變量中取得密碼
+    receiver = "1030railgunjudeglight@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message.encode("utf-8"))    # 用utf-8 解碼
