7a782aad0f1afaf2c4cc6aa99b8a9546ae3387b4
Prosper1030 <1030railgunjudgelight@gmail.com>
2024-08-22 19:41:44 +0800

    Add links, specify language limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import requests
 from send_email import send_email
+
+topic = "tesla"
+
 api_key ="2abfd9f0bfdc4a428029254b35b19434"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-07-21&sortBy=publishedAt&apiKey=2abfd9f0bfdc4a428029254b35b19434"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&" \
+      "apiKey=2abfd9f0bfdc4a428029254b35b19434&"\
+      "language=en"
 
 request = requests.get(url)
 content = request.json()
 
+print(content)
 body = ""
 
-for article in content["articles"]:
-    title = article["title"] if article["title"] is not None else ""
-    description = article["description"] if article["description"] is not None else ""
-    body = body + title + "\n" + description + 2 * "\n"
+for article in content["articles"][:20]:
+    title = article.get("title", "No Title")
+    description = article.get("description", "No Description")
+    url = article.get("url", "No URL")
+    if title and description:  # 確保都不是 None 或空字符串
+        body += f"{title}\n{description}\n{url}\n\n"
 
-send_email(body)
+send_email(subject="Today's news", message=body)
--- send_email.py
+++ send_email.py
@@ -1,16 +1,26 @@
 import smtplib
 import ssl
 import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
-def send_email(message):
+def send_email(subject ,message):
     host = "smtp.gmail.com"
     port = 465
     username = "1030railgunjudeglight@gmail.com"
     password = os.getenv("PASSWORD")   # 從環境變量中取得密碼
     receiver = "1030railgunjudeglight@gmail.com"
-    context = ssl.create_default_context()
 
+    # 創建郵件對象
+    msg = MIMEMultipart()
+    msg['From'] = username
+    msg['To'] = receiver
+    msg['Subject'] = subject
+    # 添加郵件正文
+    msg.attach(MIMEText(message, 'plain', 'utf-8'))
+
+    context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
-        server.sendmail(username, receiver, message.encode("utf-8"))    # 用utf-8 解碼
+        server.sendmail(username, receiver,  msg.as_string())
